d89753bc4ccfddfbe8e61aafeef553ead2896357
AP <178260003+apfine@users.noreply.github.com>
2025-02-08 08:38:25 +0530

    setting up auth

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const session = require("express-session")
 const express = require("express")
 const users = require("./backend/data/MOCK_DATA.json")
 const fs = require("fs")
@@ -12,11 +13,16 @@ const KEY = process.env.JWT_SECRET||"supersecret"
 
 const app = express()
 
+app.use(session({
+    secret:"supersecretkey",
+    resave:false,
+    saveUninitialized:false,
+    cookie:{httpOnly:true , secure: proccess.env.NODE_ENV==="development"}      //change to production
+}))
 app.use(express.static(path.join(__dirname , "public"))) //one common mistake is not using index.html and the system do not detects.
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
-//app.use(cookieParser())
-
+app.use(cookieParser())
 
 app.route('/api/users').get((req , res)=>{
     const html = add.text();
@@ -28,8 +34,6 @@ app.route('/api/users').get((req , res)=>{
     return res.json(body);
 })
 
-
-
 app.route('/api/users/:id').get((req , res)=>{
     const id = Number(req.params.id)
     const user = users.find((user)=>user.id===id)
@@ -38,6 +42,22 @@ app.route('/api/users/:id').get((req , res)=>{
     const id = Number(req.params.id)
     const user = users.find(user=>user.id===id)
     res.json({status:"pending"})
+})
+
+app.get("/set-cookie" , (req , res)=>{
+    if(req.cookies.mode==1){
+        res.cookie("mode" , 0 , {
+            httpOnly : true ,                                   //do not lets javascript access the cookies
+            sameSite  : "Strict",                               //helps prevent crossSite cookie attacks
+            secure: process.env.NODE_ENV ==="development"       //update it to production when shipping
+        })
+    }
+})
+
+app.get("/get-cookie" , (req , res)=>{                          //Have to create this method because we resetricted cookie access
+    const cookie = req.cookies.mode
+    if(!cookie)return null
+    else return res.send(cookie)
 })
 
 app.listen(port , ()=>{
