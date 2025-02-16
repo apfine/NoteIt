582f9f69b68c452f2d662e6d41e2f3472058b023
AP <178260003+apfine@users.noreply.github.com>
2025-02-16 11:08:42 +0530

    Commited all

--- server.js
+++ server.js
@@ -132,8 +132,7 @@ app.get("/get-cookie" , (req , res)=>{                          //Have to create
 const verifyToken = (req , res , next) =>{
 
     
-    const token = req.headers["authorization"]
-
+    const token = req.cookies?.token || req.headers.authorization?.split(" ")[1];
     if(!token) return res.status(401).json({message:"Access denied. Please login again."})
 
     try{
