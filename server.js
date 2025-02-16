8a402087c151c223bd029fbc9d5a27f8938f9d72
AP <178260003+apfine@users.noreply.github.com>
2025-02-16 13:27:59 +0530

    Updating the data pipeline

--- server.js
+++ server.js
@@ -155,14 +155,16 @@ app.post("/upload-svg" ,upload.none(), async (req , res)=>{
         const {svgD} = {...req.body , ...req.query};
         console.log("The data is : ", svgD)
         const userId = req.user.userId
-        const exists = await user.findOne({_id}).lean()
+        const exists = await user.findOne({_id:userId}).lean()
         console.log("The user is : " , exists)
-        const newSVG = new SVG({userId:exists._id, svgData:svgD})
+        const newSVG = new SVG ({userId:exists._id, svgData:svgD})
+        console.log("The model being used : " , newSVG.constructor.modelName)
         console.log("I reached here")
         await newSVG.save()
         return res.status(200).json({message:"success"})
     }
     catch(error){
+        console.log("The message : " , error)
         return res.status(500).json({message:error})
     }
 })
